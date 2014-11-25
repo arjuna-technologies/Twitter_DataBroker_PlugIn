6239a8aca0349ea88c46c6a7dda2799a150dbbcc
Stuart Wheater <stuart.wheater@arjuna.com>
2014-11-25 12:57:54 +0000

    Updated test

--- twitter-plugin/src/test/java/com/arjuna/dbplugins/twitter/test/SimpleTest.java
+++ twitter-plugin/src/test/java/com/arjuna/dbplugins/twitter/test/SimpleTest.java
@@ -4,11 +4,16 @@
 
 package com.arjuna.dbplugins.twitter.test;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.HashMap;
+import org.json.JSONObject;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import com.arjuna.databroker.data.connector.ObservableDataProvider;
+import com.arjuna.databroker.data.connector.ObserverDataConsumer;
 import com.arjuna.databroker.data.jee.DataFlowNodeLifeCycleControl;
+import com.arjuna.dbutilities.testsupport.dataflownodes.dummy.DummyDataProcessor;
 import com.arjuna.dplugins.twitter.TwitterDataSource;
 
 public class SimpleTest
@@ -17,7 +22,7 @@ public class SimpleTest
     public void simpleInvocation()
     {
         AuthenticationProperties authenticationProperties = new AuthenticationProperties("authentication.properties");
- 
+
         String              name       = "Twitter Data Source";
         Map<String, String> properties = new HashMap<String, String>();
         properties.put(TwitterDataSource.TWITTER_CONSUMERKEY_PROPERTYNAME,    authenticationProperties.getConsumerKey());
@@ -25,22 +30,27 @@ public class SimpleTest
         properties.put(TwitterDataSource.TWITTER_TOKEN_PROPERTYNAME,          authenticationProperties.getToken());
         properties.put(TwitterDataSource.TWITTER_TOKENSECRET_PROPERTYNAME,    authenticationProperties.getTokenSecret());
         properties.put(TwitterDataSource.TWITTER_TRACKTERM_PROPERTYNAME,      "newcastle");
-        properties.put(TwitterDataSource.POLLINTERVAL_PROPERTYNAME,           "4");
+        properties.put(TwitterDataSource.POLLINTERVAL_PROPERTYNAME,           "5");
 
-        TwitterDataSource twitterDataSource = new TwitterDataSource(name, properties);
+        TwitterDataSource  twitterDataSource  = new TwitterDataSource(name, properties);
+        DummyDataProcessor dummyDataProcessor = new DummyDataProcessor("Dummy Data Processor", Collections.<String, String>emptyMap());
 
-//        DataFlowNodeLifeCycleControl.processCreatedDataFlowNode(twitterDataSource, null);
+        DataFlowNodeLifeCycleControl.processCreatedDataFlowNode(twitterDataSource, null);
+        DataFlowNodeLifeCycleControl.processCreatedDataFlowNode(dummyDataProcessor, null);
 
-        twitterDataSource.startup();
-        twitterDataSource.activate();
+        ((ObservableDataProvider<String>) twitterDataSource.getDataProvider(String.class)).addDataConsumer((ObserverDataConsumer<String>) dummyDataProcessor.getDataConsumer(String.class));
+
         try
         {
-            Thread.sleep(12000);
+            Thread.sleep(60000);
         }
-        catch (Throwable throwable)
+        catch (InterruptedException interruptedException)
         {
+            fail("Interrupted during sleep");
         }
-        twitterDataSource.deactivate();
-        twitterDataSource.shutdown();
+
+        DataFlowNodeLifeCycleControl.removeDataFlowNode(dummyDataProcessor);
+
+        assertTrue("Didn't receive any tweets", dummyDataProcessor.receivedData().size() > 0);
     }
 }
